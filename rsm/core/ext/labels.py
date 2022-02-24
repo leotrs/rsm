7626363a485b8eeb5ae8dc7359ea149511b5a61e
leotrs <leo@leotrs.com>
2022-02-24 22:34:48 +0100

    feat: we can now label entire sections

--- rsm/core/ext/labels.py
+++ rsm/core/ext/labels.py
@@ -27,13 +27,43 @@ class ResolvePendingLabels(SphinxTransform):
     def apply(self, **kwargs):
         for node in self.document.traverse(pending_label):
             assert isinstance(node.parent, nodes.Element)
-            node.parent['ids'].append(node.label)
-            register_as_target(node.parent, self.env)
+
+            # Because of the way ReST works, when we use the RSM syntax
+            #
+            # section title
+            # -------------
+            # :label`some-label`
+            #
+            # sphinx creates a paragraph whose only child is a pending_label node.  This
+            # paragraph is the second child of the section node (the first child is the
+            # title).  The following code checks if the current label is within an
+            # otherwise empty paragraph that is the second child of the section.  If so,
+            # we may assume that the current label is meant to label the entire section.
+            # Otherwise, asume the user meant to label the paragraph it is part of.
+
+            paragraph = node.parent
+            section = node.parent.parent
+
+            link_text = None
+            if (
+                    isinstance(node.parent.parent, nodes.section)
+                    and paragraph.children == [node]
+                    and section.children[1] == paragraph
+            ):
+                section['ids'].append(node.label)
+                to_register = section
+                paragraph.replace_self([])
+                link_text = section.children[0].astext()
+
+            else:
+                paragraph['ids'].append(node.label)
+                to_register = paragraph
+
+            register_as_target(to_register, self.env, link_text)
             node.replace_self([])
 
 
 def setup(app):
-    # app.connect('doctree-read', register_labels)
     app.add_role('label', label_role)
     app.add_transform(ResolvePendingLabels)
 

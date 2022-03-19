bf385d0c76c2373f3d94804e077568b48ff0d759
leotrs <leo@leotrs.com>
2022-03-19 20:38:27 +0100

    first pass at a Vuejs app for rsm-edit

---
+++ rsm/rsm_edit/src/components/Editor.vue
@@ -0,0 +1,34 @@
+<script>
+ export default {
+     data() {
+         return {
+             value: "type RSM here"
+         }
+     },
+     methods: {
+         make(event) {
+             console.log('emitting make event');
+             this.$emit('make', this.value);
+         }
+     }
+ }
+</script>
+
+<template>
+  <div class="editor">
+    <textarea id="input" v-model="value"/>
+    <button @click="make">Make!</button>
+  </div>
+</template>
+
+<style scoped>
+ .editor {
+     height: 100%;
+     width: 50%;
+ }
+ 
+ #input {
+     height: 100%;
+     width: 100%;
+ }
+</style>
---
+++ rsm/rsm_edit/src/components/Viewer.vue
@@ -0,0 +1,18 @@
+<script>
+ export default {
+     props: ['body']
+ }
+</script>
+
+<template>
+  <div class="viewer">
+    <div class="manuscritp" v-html="body"></div>
+  </div>
+</template>
+
+<style scoped>
+ .viewer {
+     height: 100%;
+     width: 50%;
+ }
+</style>

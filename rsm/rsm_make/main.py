bbbf12939f4069e2c316dc6b0f8b43f38525730b
leotrs <leo@leotrs.com>
2022-04-05 16:03:50 +0200

    add live server and autoreload

--- rsm/rsm_make/main.py
+++ rsm/rsm_make/main.py
@@ -6,19 +6,36 @@ Main RSM command line utility.
 
 """
 
+import sys
+from pathlib import Path
 from argparse import ArgumentParser
 
+import livereload
 import rsm
 
 
-def make(file=None):
-    if file is None:
-        parser = ArgumentParser()
-        parser.add_argument('file', help='file to parse')
-        args = parser.parse_args()
-        file = args.file
+def make(file):
     return rsm.Application(srcpath=file).run()
 
 
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('file', help='RSM source to parse')
+    parser.add_argument('--serve', help='serve and autoreload', action='store_true')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    if args.serve:
+        cmd = f'rsm-make {args.file}'
+        server = livereload.Server()
+        server.watch(args.file, livereload.shell(cmd))
+        server.serve(root='index.html')
+    else:
+        return make(args.file)
+
+
 if __name__ == '__main__':
-    make()
+    main()

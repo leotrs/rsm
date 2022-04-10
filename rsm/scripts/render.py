6e46b4090e07b298b44ce251e02cce633815195e
leotrs <leo@leotrs.com>
2022-04-10 15:59:30 +0200

    add rsm-render support for reading from files

--- rsm/scripts/render.py
+++ rsm/scripts/render.py
@@ -17,13 +17,19 @@ def render(source):
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('src', help='RSM string to render')
+    parser.add_argument('-f', '--file', help='read source from file', action='store_true', default=False)
     args = parser.parse_args()
     return args
 
 
 def main():
     args = parse_args()
-    body = render(args.src)
+    if args.file:
+        with open(args.src) as file:
+            src = file.read()
+    else:
+        src = args.src
+    body = render(src)
     print(body)
     return 0
 

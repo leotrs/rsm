59c3edf7ab7bf1e2e2b154b3ecfcc8136e7af24d
leotrs <leo@leotrs.com>
2023-01-02 16:45:42 +0100

    refactor rsmlogger and linter

--- rsm/linter.py
+++ rsm/linter.py
@@ -16,34 +16,18 @@ main_logger = logging.getLogger("RSM")
 logger = logging.getLogger("RSM").getChild("Linter")
 
 
-LINT_LVL = 25
-
-
 class Linter:
+    LINT_LVL = 25
+
     def __init__(self) -> None:
         self.tree: Optional[nodes.Manuscript] = None
-        logging.LINT = LINT_LVL
-        logging.addLevelName(LINT_LVL, "LINT")
+        logging.LINT = self.LINT_LVL
+        logging.addLevelName(self.LINT_LVL, "LINT")
         main_logger.lint = lambda msg, *args, **kwargs: main_logger.log(
-            LINT_LVL, msg, *args, **kwargs
+            self.LINT_LVL, msg, *args, **kwargs
         )
-        if main_logger.level > LINT_LVL:
-            main_logger.level = LINT_LVL
-        target = logging.StreamHandler()
-        target.setLevel(LINT_LVL)
-        # target.setFormatter(RSMFormatter())
-        self.handler = GatherHandler([logging.WARNING, LINT_LVL], target)
-        self.handler.setLevel(LINT_LVL)
-        main_logger.addHandler(self.handler)
-
-    def flush(self) -> None:
-        if not self.handler.buffer:
-            print("No linting messages")
-            return
-        print()
-        print("------ Start of linting output ------ ")
-        self.handler.flush()
-        print("------ End of linting output ------")
+        if main_logger.level > self.LINT_LVL:
+            main_logger.level = self.LINT_LVL
 
     def lint(self, tree: nodes.Manuscript) -> nodes.Manuscript:
         main_logger.info("Linting...")

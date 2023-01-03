8679b062790774f8a52a60e737293c4c7c3a64e7
leotrs <leo@leotrs.com>
2023-01-03 19:53:52 +0100

    this should work, no?

--- docs/source/rtd_conf.py
+++ docs/source/rtd_conf.py
@@ -6,12 +6,6 @@ Production-ready sphinx configuration.
 
 """
 
-import sys
-
-sys.path.append("..")
-
 from conf import *
 
-html_static_path = ["../_static"]
-html_logo = "../_static/logo.svg"
 rsm_build_prod = True

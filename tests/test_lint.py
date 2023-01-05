02861a767f55634100fcba88162b37cd788a04ac
leotrs <leo@leotrs.com>
2023-01-05 15:46:30 +0100

    add new tests for linter and do some cleanup

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -1,18 +1,17 @@
+import re
 import subprocess
-import sys
-from importlib.metadata import version
-from textwrap import dedent
 
 import pytest
+import ujson as json
 
 EMPTY_MANUSCRIPT = ":manuscript: ::"
 EMPTY_MANUSCRIPT_LOGS = [
     {
-        "name": "RSM.parse",
-        "level": "WRN",
-        "msg": "The CST contains errors.",
-        "filename": "tsparser.py",
-        "lineno": 381,
+        "name": "RSM",
+        "level": "LINT",
+        "msg": "Manuscript with no title",
+        "filename": "linter.py",
+        "lineno": 25,
     }
 ]
 
@@ -24,74 +23,65 @@ WRONG_MANUSCRIPT_LOGS = [
         "msg": "The CST contains errors.",
         "filename": "tsparser.py",
         "lineno": 381,
-    }
+    },
+    {
+        "name": "RSM",
+        "level": "LINT",
+        "msg": "Manuscript with no title",
+        "filename": "linter.py",
+        "lineno": 25,
+    },
 ]
 
 
-def cmd(src):
-    return " ".join(["rsm-lint", f"'{src}'"])
+def cmd(src: str, log_format: str, verbose: int = 0):
+    args = [
+        "rsm-lint",
+        f'"{src}"',
+        "-c",  # interpret source as string, not path
+        f"--log-format {log_format}",  # log format
+        "--log-exclude-time",  # no timestamps
+    ]
+    if verbose:
+        args.append("-" + ("v" * verbose))
+    return " ".join(args)
 
 
-@pytest.mark.slow
-def test_render(tmp_path):
-    want = dedent(
-        """
-        <body>
-
-        <div class="manuscriptwrapper">
-
-        <div class="manuscript">
-
-        <section class="level-1">
-
-        <p class="paragraph">Foo.</p>
-
-        <p class="paragraph">Bar.</p>
-
-        <p class="paragraph">Baz.</p>
-
-        </section>
-
-        </div>
-
-        </div>
-
-        </body>
-        """
-    )
-    # In this source code, we use the \r\n line ending typical in Windows systems.  When
-    # running this test on a UNIX machine, they will simply be ignored.  When running on
-    # a Windows machine, the test will fail if they are not present.
-    src = ":manuscript:\r\n\r\nFoo.\r\n\r\nBar.\r\n\r\nBaz.\r\n\r\n::\r\n"
-    file = tmp_path / "test.rsm"
-    file.write_text(src)
+def run(src: str, log_format: str, verbose: int = 0, replace=False, split=False):
     result = subprocess.run(
-        f"rsm-render {str(file.resolve())}",
-        stdout=subprocess.PIPE,
+        cmd(src, log_format, verbose),
         check=True,
         shell=True,
-    )
-
-    # get rid of any logs appearing before the html body, and decode
-    output = result.stdout[result.stdout.find(b"<body>") :].decode("utf-8")
-    if sys.platform == "win32":
-        output = output.replace("\r", "")
-    assert output.strip() == want.strip()
-
-
-@pytest.mark.slow
-def test_invalid_rsm():
-    have = "test_file.rsm"
-    with pytest.raises(subprocess.CalledProcessError):
-        result = subprocess.run(
-            cmd(have), stdout=subprocess.PIPE, check=True, shell=True
-        )
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    ).stdout.decode("utf-8")
+    if replace:
+        result = re.sub(r"}\s*{", "},{", result)
+    if split:
+        result = [l.strip() for l in result.strip().split("\n")]
+    return result
 
 
 @pytest.mark.slow
-def test_render_version():
-    result = subprocess.run(
-        "rsm-render --version", stdout=subprocess.PIPE, check=True, shell=True
-    )
-    output = result.stdout.decode("utf-8").strip()
-    assert output == f"rsm-markup v{version('rsm-markup')}"
+def test_json_logs_of_empty_manuscript():
+    have = [json.loads(run(EMPTY_MANUSCRIPT, "json"))]
+    assert have == EMPTY_MANUSCRIPT_LOGS
+
+
+@pytest.mark.slow
+def test_plain_logs_of_empty_manuscript():
+    have = run(EMPTY_MANUSCRIPT, "plain").strip()
+    assert have == EMPTY_MANUSCRIPT_LOGS[0]["msg"]
+
+
+@pytest.mark.slow
+def test_json_logs_of_wrong_manuscript():
+    output = run(WRONG_MANUSCRIPT, "json", replace=True)
+    have = json.loads(f"[{output}]")
+    assert have == WRONG_MANUSCRIPT_LOGS
+
+
+@pytest.mark.slow
+def test_plain_logs_of_wrong_manuscript():
+    have = run(WRONG_MANUSCRIPT, "plain", split=True)
+    assert have == [r["msg"] for r in WRONG_MANUSCRIPT_LOGS]
--- tests/test_logs.py
+++ tests/test_logs.py
@@ -4,8 +4,6 @@ import subprocess
 import pytest
 import ujson as json
 
-from rsm.app import ProcessorApp
-
 EMPTY_MANUSCRIPT = ":manuscript: ::"
 EMPTY_MANUSCRIPT_LOGS = {}
 EMPTY_MANUSCRIPT_LOGS_V = [
@@ -160,57 +158,51 @@ def run(src: str, log_format: str, verbose: int = 0, replace=False, split=False)
 
 
 @pytest.mark.slow
-def test_json_logs_of_empty_file():
+def test_json_logs_of_empty_manuscript():
     have = json.loads(run(EMPTY_MANUSCRIPT, "json") or "{}")
     assert have == EMPTY_MANUSCRIPT_LOGS
 
 
 @pytest.mark.slow
-def test_json_logs_of_empty_file_verbose():
+def test_json_logs_of_empty_manuscript_verbose():
     output = run(EMPTY_MANUSCRIPT, "json", verbose=1, replace=True)
     have = json.loads(f"[{output}]")
     assert have == EMPTY_MANUSCRIPT_LOGS_V
 
 
 @pytest.mark.slow
-def test_plain_logs_of_empty_file():
+def test_plain_logs_of_empty_manuscript():
     have = run(EMPTY_MANUSCRIPT, "plain").strip() or {}
     assert have == EMPTY_MANUSCRIPT_LOGS
 
 
 @pytest.mark.slow
-def test_plain_logs_of_empty_file_verbose():
+def test_plain_logs_of_empty_manuscript_verbose():
     have = run(EMPTY_MANUSCRIPT, "plain", verbose=1, split=True)
     assert have == [r["msg"] for r in EMPTY_MANUSCRIPT_LOGS_V]
 
 
 @pytest.mark.slow
-def test_json_logs_of_wrong_file():
+def test_json_logs_of_wrong_manuscript():
     output = run(WRONG_MANUSCRIPT, "json", replace=True)
     have = json.loads(f"[{output}]")
     assert have == WRONG_MANUSCRIPT_LOGS
 
 
 @pytest.mark.slow
-def test_json_logs_of_wrong_file_verbose():
+def test_json_logs_of_wrong_manuscript_verbose():
     output = run(WRONG_MANUSCRIPT, "json", verbose=1, replace=True)
     have = json.loads(f"[{output}]")
     assert have == WRONG_MANUSCRIPT_LOGS_V
 
 
 @pytest.mark.slow
-def test_plain_logs_of_wrong_file():
+def test_plain_logs_of_wrong_manuscript():
     have = run(WRONG_MANUSCRIPT, "plain", split=True)
     assert have == [r["msg"] for r in WRONG_MANUSCRIPT_LOGS]
 
 
 @pytest.mark.slow
-def test_plain_logs_of_wrong_file_verbose():
+def test_plain_logs_of_wrong_manuscript_verbose():
     have = run(WRONG_MANUSCRIPT, "plain", verbose=1, split=True)
     assert have == [r["msg"] for r in WRONG_MANUSCRIPT_LOGS_V]
-
-
-# # we need to set (and later unset?) the logger at run()-time, not at instantiation time
-# # test using the following:
-# aj = ProcessorApp(plain=source, log_time=False, log_format="json")
-# ar = ProcessorApp(plain=source, log_time=False, log_format="rsm")

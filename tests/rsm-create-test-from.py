68a052abdefb0296dbaed3a8bad3d8883e813774
leotrs <leo@leotrs.com>
2023-01-11 08:31:30 +0100

    update tests and run black

--- tests/rsm-create-test-from.py
+++ tests/rsm-create-test-from.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python3
 import re
 import sys
-import rsm
-from pathlib import Path
 from argparse import ArgumentParser
+from pathlib import Path
 from textwrap import indent
+
+import rsm
 
 TEMPLATE = """
 
@@ -18,47 +19,47 @@ def {test_name}():
         \"\"\",
     )
 """
-ROOT_TEST_PATH = Path(rsm.__file__).parent.parent / 'tests'
+ROOT_TEST_PATH = Path(rsm.__file__).parent.parent / "tests"
 
 
 def main(file_path, test_path, test_name, handrails=False):
     """Turn a text file into a test."""
     out_path = ROOT_TEST_PATH / test_path
-    assert out_path.exists()
+    assert out_path.exists(), f"Cannot find test file {out_path}"
 
-    with open(out_path, encoding='utf-8') as file:
+    with open(out_path, encoding="utf-8") as file:
         current_tests = file.read()
-    if re.search(f'^def {test_name}\(', current_tests, flags=re.MULTILINE):
-        raise ValueError('Test with that name already exists')
+    if re.search(f"^def {test_name}\(", current_tests, flags=re.MULTILINE):
+        raise ValueError("Test with that name already exists")
 
-    with open(file_path, encoding='utf-8') as file:
+    with open(file_path, encoding="utf-8") as file:
         source = file.read()
     target = rsm.render(source, handrails=handrails)
 
-    with open(out_path, 'a', encoding='utf-8') as file:
+    with open(out_path, "a", encoding="utf-8") as file:
         file.write(
             TEMPLATE.format(
                 test_name=test_name,
-                source=indent(source.strip(), '        '),
-                target=indent(target.strip(), '        '),
+                source=indent(source.strip(), "        "),
+                target=indent(target.strip(), "        "),
             )
         )
 
     sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('file_path', help='path to RSM file')
+    parser.add_argument("file_path", help="path to RSM file")
     parser.add_argument(
-        'test_path', help='path to test module relative to rsm/rsm/tests'
+        "test_path", help="path to test module relative to rsm/rsm/tests"
     )
-    parser.add_argument('test_name', help='name of new test')
+    parser.add_argument("test_name", help="name of new test")
     parser.add_argument(
-        '-r',
-        '--handrails',
-        help='with handrails',
-        action='store_true',
+        "-r",
+        "--handrails",
+        help="with handrails",
+        action="store_true",
         default=False,
     )
     args = parser.parse_args()

e15e04e7ca7b2cd5907b27085736e3fb20eca9ac
Leo Torres <leo.torres@fgsglobal.com>
2024-05-04 19:06:03 +0200

    make a sidebar with a nice tabler icon, but not while testing

--- rsm/translator.py
+++ rsm/translator.py
@@ -1078,6 +1078,11 @@ class HandrailsTranslator(Translator):
         <path d="M6 5l-2 2l2 2"></path>
         <path d="M10 9l2 -2l-2 -2"></path>
         </svg>""",
+        "vars": """<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-variable">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M5 4c-2.5 5 -2.5 10 0 16m14 -16c2.5 5 2.5 10 0 16m-10 -11h1c1 0 1 1 2.016 3.527c.984 2.473 .984 3.473 1.984 3.473h1" />
+        <path d="M8 16c1.5 0 3 -2 4 -3.5s2.5 -3.5 4 -3.5" />
+        </svg>""",
     }
 
     def __init__(self, quiet: bool = False, hidden_handrails: bool = True, sidebar: bool = True):
@@ -1158,12 +1163,16 @@ class HandrailsTranslator(Translator):
             0, [cmd], AppendBatchAndDefer, include_content
         )
 
+    def _make_tools_sidebar(self):
+        return AppendText(text=f'<div class="tools-sidebar">{self.svg["vars"]}</div>')
+
     def visit_manuscript(self, node: nodes.Manuscript) -> EditCommand:
         batch = super().visit_manuscript(node)
         if node.title:
-            return self._replace_batch_with_handrails(4, batch)
-        else:
-            return batch
+            batch = self._replace_batch_with_handrails(4, batch)
+        if self.sidebar:
+            batch.items.insert(1, self._make_tools_sidebar())
+        return batch
 
     def visit_section(self, node: nodes.Section) -> EditCommand:
         batch = super().visit_section(node)

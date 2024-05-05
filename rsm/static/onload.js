b9189d636f3646eddf3d7f74dd3a4f8e98827e97
Leo Torres <leo.torres@fgsglobal.com>
2024-05-05 17:26:16 +0200

    make a connection to the language server and expose it to the entire window by returning it

--- rsm/static/onload.js
+++ rsm/static/onload.js
@@ -3,7 +3,10 @@
 // Function to run when the entire document finishes loading
 //
 
+
+
 export function onload(path = "/static/") {
+    let lsp_ws;
     import(path + 'libraries.js').then((libs) => {
         libs.loadMathJax().then(() => {
             console.log('MathJax loaded!');
@@ -33,5 +36,43 @@ export function onload(path = "/static/") {
 	    console.error('Loading MathJax FAILED!');
 	    console.error(err);
         })
-    })
+    });
+
+    lsp_ws = new WebSocket("ws://127.0.0.1:1234");
+    lsp_ws.onmessage = function(event) {console.log(`[message] Data received from server: ${event.data}`)};
+    lsp_ws.onerror = function(error) {console.log(`[error]`)};
+    lsp_ws.onclose = function(event) {
+        if (event.wasClean) {
+            console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+        } else {
+            console.log('[close] Connection died');
+        }
+    };
+    lsp_ws.onopen = function(e) {
+        console.log("[open] Connection established");
+        lsp_ws.send(JSON.stringify({
+            "jsonrpc": "2.0",
+            "method": "initialize",
+            "id": 1,
+            "params": {
+	        "processId": null,
+	        "capabilities":  {}
+            }
+        }));
+        const src = $("body").children(".rsm-source");
+        lsp_ws.send(JSON.stringify({
+            "jsonrpc" : "2.0",
+            "method" : "textDocument/didOpen",
+            "params": {
+                "textDocument": {
+                    "languageId": "rsm",
+                    "text": src.text(),
+                    "uri": "file://src/hello.rsm",
+                    "version": 1
+                }
+           }
+        }))
+    };
+
+    return lsp_ws;
 }

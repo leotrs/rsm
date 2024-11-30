c77e17f442aec41626016e9c5a97b71ecd3908c0
Leo Torres <leo@leotrs.com>
2024-11-30 21:33:06 +0100

    docs

--- rsm/linter.py
+++ rsm/linter.py
@@ -19,7 +19,6 @@ class Linter:
     LINT_LVL = 25
 
     def __init__(self) -> None:
-        self.tree: Optional[nodes.Manuscript] = None
         logging.LINT = self.LINT_LVL
         logging.addLevelName(self.LINT_LVL, "LINT")
         logger.lint = lambda msg, *args, **kwargs: logger.log(
@@ -29,24 +28,30 @@ class Linter:
             logger.level = self.LINT_LVL
 
     def lint(self, tree: nodes.Manuscript) -> nodes.Manuscript:
+        """Lint the manuscript."""
         logger.info("Linting...")
-        self.tree = tree
+        self.tree: nodes.Manuscript = tree
 
         self.lint_manuscript_title()
         self.lint_section_label()
 
-    def _extra(self, node):
+        return self.tree
+
+    def _extra(self, node: nodes.Node) -> dict:
+        """Make the extra arguments for the logger.lint call."""
+        # Most editors start counting lines of code at 1, but TS starts at 0
         return {
             "start_row": node.start_point[0] + 1,
             "start_col": node.start_point[1] + 1,
         }
 
-    def lint_manuscript_title(self):
+    def lint_manuscript_title(self) -> None:
+        """Manuscript should have a title."""
         if not self.tree.title:
             logger.lint("Manuscript with no title", extra=self._extra(self.tree))
-        return self.tree
 
-    def lint_section_label(self):
+    def lint_section_label(self) -> None:
+        """Sections should be labeled."""
         for section in self.tree.traverse(nodeclass=nodes.Section):
             if not section.label:
                 logger.lint("Section with no label", extra=self._extra(section))

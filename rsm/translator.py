c9a9778cac397193275f49356c99c2d6f4ed1a78
Leo Torres <leo@leotrs.com>
2025-01-18 17:57:16 +0100

    change the stroke of right chevron form the path to the other svg, update the translator and corresponding tests

--- rsm/translator.py
+++ rsm/translator.py
@@ -1234,9 +1234,9 @@ class HandrailsTranslator(Translator):
         if collapsible:
             content = """
             <div class="hr-collapse">
-              <div class="icon-wrapper">
-                <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M1 1L7 7L1 13" stroke="#3C4952" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <div class="icon-wrapper collapse">
+                <svg width="8" height="14" viewBox="0 0 8 14" fill="none" stroke="#3C4952" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 1L7 7L1 13" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
               </div>
             </div>

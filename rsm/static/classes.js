2debe5774a3d8b298b5f5fe7c5ab6a61ae56677c
Leo Torres <leo@leotrs.com>
2025-01-24 14:59:47 +0100

    better JS using ResizeObserver and better translation with a couple of custom methods for steps and subproofs

--- rsm/static/classes.js
+++ rsm/static/classes.js
@@ -23,50 +23,15 @@ export function setupClassInteractions() {
 
     // Collapse
     document.querySelectorAll(".hr > .hr-collapse-zone > .hr-collapse").forEach(collapse => {
-	collapse.addEventListener("click", function () {
-            const hr = this.parentElement.parentElement;
-	    let rest;
-	    if (hr.classList.contains("hr-labeled")) {
-		rest = hr.querySelectorAll(".hr-content-zone > :not(.hr-label)");
-	    } else {
-		rest = Array.from(hr.parentElement.children).filter(el => {return el !== hr});
-		console.log(Array.from(hr.parentElement.children));
-	    };
-
-	    const icon = hr.querySelector(".hr-collapse-zone > .hr-collapse > .icon-wrapper");
-	    if (!hr.classList.contains("hr-collapsed")) {
-		hr.classList.add("hr-collapsed");
-		rest.forEach(el => { el.classList.add("hide"); });
-		icon.classList.remove("collapse");
-		icon.classList.add("uncollapse");
-		icon.innerHTML = `
-                    <svg width="14" height="8" viewBox="0 0 14 8" fill="none" stroke="#3C4952" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1 1L7 7L13 1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    `;
-	    } else {
-		hr.classList.remove("hr-collapsed");
-		rest.forEach(el => { el.classList.remove("hide"); });
-		icon.classList.remove("uncollapse");
-		icon.classList.add("collapse");
-		icon.innerHTML = `
-                    <svg width="8" height="14" viewBox="0 0 8 14" fill="none" stroke="#3C4952" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1 1L7 7L1 13" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    `;
-	    };
-
-	});
+	collapse.addEventListener("click", collapseHandrail);
+    });
+    document.querySelectorAll(".hr.step > .hr-menu-zone > .hr-menu > .hr-menu-item.collapse-subproof:not(.disabled)").forEach(collapse => {
+	collapse.addEventListener("click", collapseHandrail);
     });
 
     // Set height of offset handrails' borders
-    document.querySelectorAll('.hr.hr-offset > .hr-border-zone > .hr-border-rect').forEach(border => {
-	const siblings = Array.from(border.parentElement.parentElement.children);
-	const content = siblings.find(sibling => sibling.classList.contains("hr-content-zone"));
-	if (content) {
-	    border.style.height = `${content.offsetHeight}px`;
-	}
-    });
+    const resizeObserver = new ResizeObserver(updateHeight);
+    document.querySelectorAll('.hr.hr-offset > .hr-content-zone').forEach(el => resizeObserver.observe(el));
 
     // Minimap
     const items = document.querySelectorAll('ul.contents li.item');
@@ -78,5 +43,61 @@ export function setupClassInteractions() {
 	    document.getElementById("stop-follow-mouse-2").setAttribute("offset", `${percent}%`);
 	});
     });
+
+}
+
+
+function updateHeight(entries) {
+    for (const entry of entries) {
+	console.log(`resized ${entry}`);
+	const hr = entry.target.parentElement;
+	const elementsToResize = hr.querySelectorAll('& > .hr-border-zone, & > .hr-spacer-zone');
+	elementsToResize.forEach(el => { el.style.height = `${entry.contentRect.height}px`; })
+    }
+};
+
+
+function collapseHandrail() {
+    const hr = this.closest(".hr");
+    let rest;
+    if (hr.classList.contains("hr-labeled")) {
+	rest = hr.querySelectorAll("& > .hr-content-zone > :not(.hr-label)");
+    } else if (hr.classList.contains("step")) {
+	rest = hr.querySelectorAll("& > .hr-content-zone > :not(.statement)");
+    } else {
+	rest = Array.from(hr.parentElement.children).filter(el => {return el !== hr});
+    };
+
+    if (!hr.classList.contains("hr-collapsed")) {
+	hr.classList.add("hr-collapsed");
+	rest.forEach(el => { el.classList.add("hide"); });
+	const icon = hr.querySelector("& .icon-wrapper.collapse");
+	if (icon) {
+	    icon.classList.remove("collapse");
+	    icon.classList.add("uncollapse");
+	    icon.innerHTML = `
+                    <svg width="14" height="8" viewBox="0 0 14 8" fill="none" stroke="#3C4952" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M1 1L7 7L13 1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    `;
+	    const menu_item = icon.nextElementSibling;
+	    if (menu_item.classList.contains("hr-menu-item-text")) { menu_item.textContent = "Expand" };
+	}
+    } else {
+	hr.classList.remove("hr-collapsed");
+	rest.forEach(el => { el.classList.remove("hide"); });
+	const icon = hr.querySelector("& .icon-wrapper.uncollapse");
+	if (icon) {
+	    icon.classList.remove("uncollapse");
+	    icon.classList.add("collapse");
+	    icon.innerHTML = `
+                    <svg width="8" height="14" viewBox="0 0 8 14" fill="none" stroke="#3C4952" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M1 1L7 7L1 13" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    `;
+	    const menu_item = icon.nextElementSibling;
+	    if (menu_item.classList.contains("hr-menu-item-text")) { menu_item.textContent = "Collapse" };
+	}
+    };
 
 }
